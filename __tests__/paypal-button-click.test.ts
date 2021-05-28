7346f318d39e0255412c30bfc54488f94393de0c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-05-28 14:07:48 -0500

    Move to mocha

--- __tests__/paypal-button-click.test.ts
+++ __tests__/paypal-button-click.test.ts
@@ -1,49 +1,37 @@
-import { remote, Browser } from "webdriverio";
-import { config } from "../wdio.conf";
+import { expect } from "chai";
 
-let browser: Browser<"async">;
-
-beforeAll(async () => {
-  browser = await remote(config);
-});
-
-afterAll(async () => {
-  await browser.deleteSession();
-});
-
-test("should start payment flow when clicking on paypal button", async () => {
-  await browser.url(
-    "https://developer.paypal.com/demo/checkout/#/pattern/client"
-  );
-  expect(await browser.getTitle()).toEqual("Smart Payment Buttons Integration");
-
-  const frame = await browser.$("div#paypal-button-container iframe");
-
-  await browser.switchToFrame(frame);
-  const paypalButton = await browser.$('[data-funding-source="paypal"]');
-  await paypalButton.waitForDisplayed();
-  expect(await paypalButton.isDisplayed()).toEqual(true);
-
-  await paypalButton.click();
-  await browser.switchToParentFrame();
-
-  let windows: string[] = await browser.getWindowHandles();
-
-  await browser.waitUntil(async () => {
-    if (windows.length >= 2) {
-      return true;
-    }
-
-    windows = await browser.getWindowHandles();
-    return false;
+describe("Testing Paypal buttons", () => {
+  beforeEach(async () => {
+    await browser.url(
+      "https://developer.paypal.com/demo/checkout/#/pattern/client"
+    );
   });
 
-  expect(windows.length).toEqual(2);
+  it("should start payment flow when clicking on paypal button", async () => {
+    const pageTitle = await browser.getTitle();
+    expect(pageTitle).to.equal("Smart Payment Buttons Integration");
 
-  await browser.switchToWindow(windows[1]);
+    const frame = await browser.$("div#paypal-button-container iframe");
 
-  const emailTextField = await browser.$("#email");
-  await emailTextField.waitForDisplayed();
+    await browser.switchToFrame(frame);
+    const paypalButton = await browser.$('[data-funding-source="paypal"]');
+    await paypalButton.waitForDisplayed();
 
-  expect(await browser.getTitle()).toEqual("Log in to your PayPal account");
+    const isButtonDisplayed = await paypalButton.isDisplayed();
+    expect(isButtonDisplayed).to.equal(true);
+
+    await paypalButton.click();
+    await browser.switchToParentFrame();
+
+    let windows: string[] = await browser.getWindowHandles();
+    expect(windows.length).to.equal(2);
+
+    await browser.switchToWindow(windows[1]);
+
+    const emailTextField = await browser.$("#email");
+    await emailTextField.waitForDisplayed();
+
+    const loginPageTitle = await browser.getTitle();
+    expect(loginPageTitle).to.equal("Log in to your PayPal account");
+  });
 });
